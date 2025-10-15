ab16087d4e023370ea29d88369b1edb2409bf65f
MJONES155 <mjones155@murraystate.edu>
2025-10-15 17:06:13 -0500

    UI Initial Commit

---
+++ lib/ui/widgets/headline.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Headline extends StatelessWidget {
+  final String title;
+  final String? caption;
+
+  const Headline({
+    super.key,
+    required this.title,
+    this.caption,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (context.width <= 600) {
+      return Padding(
+        padding: const EdgeInsets.only(left: 52.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            caption != null
+                ? Text(
+                    caption!,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge!
+                        .copyWith(fontWeight: FontWeight.bold, fontSize: 12),
+                  )
+                : const SizedBox.shrink(),
+          ],
+        ),
+      );
+    } else {
+      return caption != null
+          ? Row(
+              children: [
+                Text(
+                  title,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineMedium!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 16),
+                  height: 24,
+                  child: const VerticalDivider(indent: 0, endIndent: 0),
+                ),
+                Text(
+                  caption!,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+              ],
+            )
+          : Text(
+              title,
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineMedium!
+                  .copyWith(fontWeight: FontWeight.bold),
+            );
+    }
+  }
+}
