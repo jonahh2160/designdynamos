4dec128e6985269718f0227011bea6613be40a6d
Jonah H. <jonahhampton@tutamail.com>
2025-12-11 13:27:04 -0600

    Add primitive sign out screen

--- lib/features/auth/pages/signout_screen.dart
+++ lib/features/auth/pages/signout_screen.dart
@@ -1,9 +1,23 @@
+import 'package:designdynamos/features/auth/pages/login_page.dart';
 import "package:flutter/material.dart";
+import 'package:designdynamos/data/services/supabase_service.dart';
 
 class SignOutScreen extends StatelessWidget {
   const SignOutScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Sign Out')));
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await SupabaseService.client.auth.signOut();
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => const LoginPage()),
+            );
+          },
+          child: Text('Sign Out'),
+        ),
+      ),
+    );
   }
 }
